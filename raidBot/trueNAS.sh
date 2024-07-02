a7af3e6792a5af297961178b685c5bd39b87762b
rpassosdomingues <rafael.domingues@sou.unifal-mg.edu.br>
2024-07-02 14:16:25 -0300

    Jul 2 Tue

--- raidBot/trueNAS.sh
+++ raidBot/trueNAS.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Instala as dependências necessárias
+echo "Instalando ZFS..."
+sudo apt install -y zfsutils-linux
+
 # Variáveis de configuração
 POOL_NAME="raid0pool"
 DISK1="/dev/sda"
