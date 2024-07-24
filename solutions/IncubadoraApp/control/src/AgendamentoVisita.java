989b29e77bb8db535da165ef5ef85c94f9e62b42
rpassosdomingues <rafael.domingues@sou.unifal-mg.edu.br>
2024-07-23 22:34:59 -0300

    Jul 23 Tue

--- solutions/IncubadoraApp/control/src/AgendamentoVisita.java
+++
@@ -1,9 +0,0 @@
-public class AgendamentoVisita {
-    private LocalDateTime dataVisita;
-    private LocalTime horaInicio;
-    private LocalTime horaFim;
-    private int numeroVisitantes;
-    private String solicitante;
-
-    // getters e setters
-}
